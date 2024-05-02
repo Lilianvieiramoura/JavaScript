23078dd1cd33080b08ad7ad1ceabf001073aca02
Lílian Vieira <lilianv528@gmail.com>
2024-05-02 18:11:52 -0300

    retorno da promise rejeitada

--- promise.js
+++ promise.js
@@ -9,15 +9,23 @@ const resolvedPromise = () =>
     }, 1000);
   });
 
-resolvedPromise().then((response) => {
-  console.log(`O número gerado é ${response}`);
-})
-
 // promise rejeitada retornando um objeto de erro
-// const rejectedPromise = () =>
-//   new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       const randomNumber = generateRandomNumber();
-//       reject(new Error(`O número ${randomNumber} é inválido.`));
-//     }, 1000);
-//   });
+const rejectedPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      reject(new Error(`O número ${randomNumber} é inválido.`));
+    }, 1000);
+  });
+
+resolvedPromise().then((response) => {
+    console.log(`resolvedPromise: O número gerado é ${response}`);
+});
+
+rejectedPromise()
+  .then((response) => {
+  console.log(`rejectedPromise: O número gerado é ${response}`);
+  })
+  .catch((error) => {
+    console.log(`rejectedPromise: ${error.message}`);
+  })
